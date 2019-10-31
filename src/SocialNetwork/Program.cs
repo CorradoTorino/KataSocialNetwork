03c8b81669da4f2e3c79d1f114cf9293c6ee63c8
Corrado Calzoni <corrado.calzoni@roche.com>
2019-10-31 23:30:04 +0100

    Modifies Program main to use SocialNetworkApp

--- src/SocialNetwork/Program.cs
+++ src/SocialNetwork/Program.cs
@@ -6,7 +6,8 @@ namespace SocialNetwork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var consoleApp = new SocialNetworkConsoleApp(new SystemConsoleWrapper());
+            consoleApp.Run();
         }
     }
 }
--- src/SocialNetwork/SocialNetworkConsoleApp.cs
+++ src/SocialNetwork/SocialNetworkConsoleApp.cs
@@ -2,7 +2,14 @@ namespace SocialNetwork
 {
     public class SocialNetworkConsoleApp
     {
+        private readonly IConsole console;
+
         public SocialNetworkConsoleApp(IConsole console)
+        {
+            this.console = console;
+        }
+
+        public void Run()
         {
             console.WriteLine("a");
             console.WriteLine("b");
