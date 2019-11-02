6ee6947421900cf0edf78e7dcce1e9bfa45c9a78
Corrado Calzoni <corrado.calzoni@roche.com>
2019-11-02 23:38:54 +0100

    test simple login

--- src/SocialNetwork/SocialNetworkConsoleApp.cs
+++ src/SocialNetwork/SocialNetworkConsoleApp.cs
@@ -1,3 +1,7 @@
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace SocialNetwork
 {
     public class SocialNetworkConsoleApp
@@ -11,8 +15,38 @@ namespace SocialNetwork
 
         public void Run()
         {
+            var runTask = Task.Run(() => this.RunAsync(CancellationToken.None));
+            runTask.Wait();
+        }
+
+        public async Task RunAsync(CancellationToken token)
+        {
             console.WriteLine("Welcome to Social Network!");
-            console.WriteLine("Usage to post.. <Alice> This is my dummy post!");
+            console.WriteLine("Please, Login to start: Write your user name.");
+
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+
+                var readLineTask = this.ReadLineAsync();
+                await readLineTask;
+
+                var input = readLineTask.Result;
+
+                if (input == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    console.WriteLine($"Welcome {input}!");
+                }
+            }
+        }
+
+        private async Task<string> ReadLineAsync()
+        {
+            return await Task.Run(() => console.ReadLine());
         }
     }
 }
--- tests/AcceptanceTests/SocialNetworkConsoleAppTests.cs
+++ tests/AcceptanceTests/SocialNetworkConsoleAppTests.cs
@@ -1,3 +1,6 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SocialNetwork;
@@ -13,6 +16,7 @@ namespace AcceptanceTests
             // Arrange
             var consoleMock = new Mock<IConsole>();
             var socialNetworkConsoleApp = new SocialNetworkConsoleApp(consoleMock.Object);
+            consoleMock.Setup(m => m.ReadLine()).Returns("exit");
 
             // Act
             socialNetworkConsoleApp.Run();
@@ -27,6 +31,7 @@ namespace AcceptanceTests
             // Arrange
             var consoleMock = new Mock<IConsole>();
             var socialNetworkConsoleApp = new SocialNetworkConsoleApp(consoleMock.Object);
+            consoleMock.Setup(m => m.ReadLine()).Returns("exit");
 
             // Act
             socialNetworkConsoleApp.Run();
@@ -36,17 +41,56 @@ namespace AcceptanceTests
         }
 
         [TestMethod]
-        public void WelcomeScreen_ShowsHowToPost()
+        public void WelcomeScreen_ShowsLoginRequest()
         {
             // Arrange
             var consoleMock = new Mock<IConsole>();
             var socialNetworkConsoleApp = new SocialNetworkConsoleApp(consoleMock.Object);
+            consoleMock.Setup(m => m.ReadLine()).Returns("exit");
 
             // Act
             socialNetworkConsoleApp.Run();
 
             // Assert
-            consoleMock.Verify(m => m.WriteLine("Usage to post.. <Alice> This is my dummy post!"), Times.Once);
+            consoleMock.Verify(m => m.WriteLine("Please, Login to start: Write your user name."), Times.Once);
+        }
+
+        [TestMethod]
+        public void WelcomeScreen_AfterLogin_TheUserIsWelcome()
+        {
+            // Arrange
+            var consoleMock = new Mock<IConsole>();
+            var socialNetworkConsoleApp = new SocialNetworkConsoleApp(consoleMock.Object);
+            using var semaphore = new ManualResetEvent(false);
+            
+            var errorOccurred = false;
+            var readLines = 0;
+            consoleMock.Setup(m => m.ReadLine()).
+                Returns(() =>
+                {
+                    switch (readLines)
+                    {
+                        case 0:
+                            readLines++;
+                            return "Alice";
+                        case 1:
+                            readLines++;
+                            semaphore.Set();
+                            return "exit";
+                        default:
+                            errorOccurred = true;
+                            return "";
+                    }
+                });
+
+            // Act
+            Task.Run(() => socialNetworkConsoleApp.RunAsync(CancellationToken.None));
+
+            // Assert
+            semaphore.WaitOne();
+            Assert.IsFalse(errorOccurred);
+            consoleMock.Verify(m => m.WriteLine("Welcome Alice!"), Times.Once);
+
         }
     }
 }
