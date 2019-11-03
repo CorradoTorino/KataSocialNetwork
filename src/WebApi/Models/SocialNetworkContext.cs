e14123dac7d3a72357994d89c703e4596e77b316
Corrado Calzoni <corrado.calzoni@roche.com>
2019-11-03 23:11:46 +0100

    Add WebApi project

---
+++ src/WebApi/Models/SocialNetworkContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class SocialNetworkContext: DbContext
+    {
+        public SocialNetworkContext(DbContextOptions<SocialNetworkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
